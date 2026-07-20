b8a61ad3f443efbedfa95dddfe4713443f51c287
Vlad Pavlechko <vlad9.ru@gmail.com>
2026-07-20 13:32:28 +0300

    create db

---
+++ src/database/alchemy/connection.py
@@ -0,0 +1,38 @@
+from collections.abc import AsyncIterator
+
+from uuid_utils import uuid7
+
+from sqlalchemy import URL, NullPool
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker, AsyncSession
+
+__all__ = ["AlchemyDBConnection"]
+
+
+class AlchemyDBConnection:
+    def __init__(self, url: URL | str):
+        self.url = url
+        self.engine: AsyncEngine = create_async_engine(
+            url=self.url,
+            pool_recycle=0,
+            poolclass=NullPool,
+            query_cache_size=0,
+            connect_args={
+                "prepared_statement_name_func": lambda: f"__asyncpg_{uuid7()}__",
+                "statement_cache_size": 0,
+                "prepared_statement_cache_size": 0,
+                "command_timeout": 30,
+            },
+            execution_options={"isolation_level": "READ COMMITTED"},
+        )
+        self.session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
+
+    async def get_session(self) -> AsyncIterator[AsyncSession]:
+        async with self.session_maker() as session:
+            yield session
+
+    async def close(self) -> None:
+        await self.engine.dispose()
---
+++ src/database/alchemy/mixins/lifecycle.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.orm import declarative_mixin, Mapped, mapped_column
+from sqlalchemy.sql.functions import now
+
+__all__ = ["LifecycleMixin"]
+
+
+@declarative_mixin
+class LifecycleMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        insert_default=now,
+        nullable=False,
+        comment="Date of created",
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        insert_default=now,
+        nullable=False,
+        comment="Date of last updated",
+    )
