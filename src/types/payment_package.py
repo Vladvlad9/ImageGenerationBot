7b3679ab58334250abbff45716bec296ed70a1c5
Vlad Pavlechko <vlad9.ru@gmail.com>
2026-07-29 17:21:07 +0300

    upd TokenPackage

--- src/types/payment_package.py
+++ src/types/payment_package.py
@@ -17,9 +17,9 @@ class TokenPackage:
 
 
 TOKEN_PACKAGES = [
-    TokenPackage(tokens=5_000, stars=1, callback_data="buy_tokens_5000", payload="tokens_5000"),
-    TokenPackage(tokens=10_000, stars=10, callback_data="buy_tokens_10000", payload="tokens_10000"),
-    TokenPackage(tokens=25_000, stars=25, callback_data="buy_tokens_25000", payload="tokens_25000"),
+    TokenPackage(tokens=5_000, stars=5, callback_data="buy_tokens_5000", payload="tokens_5000"),
+    TokenPackage(tokens=10_000, stars=20, callback_data="buy_tokens_10000", payload="tokens_10000"),
+    TokenPackage(tokens=25_000, stars=40, callback_data="buy_tokens_25000", payload="tokens_25000"),
 ]
 
 TOKEN_PACKAGES_BY_CALLBACK = {package.callback_data: package for package in TOKEN_PACKAGES}
